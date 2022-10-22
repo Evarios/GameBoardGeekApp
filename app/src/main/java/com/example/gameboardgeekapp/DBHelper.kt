94f9896bf2f8145073f730a11cb09046b360b72b
Evarios <jan.brozdziak@gmail.com>
2022-10-22 19:42:32 +0200

    getting values from xml file

--- app/src/main/java/com/example/gameboardgeekapp/DBHelper.kt
+++ app/src/main/java/com/example/gameboardgeekapp/DBHelper.kt
@@ -9,7 +9,15 @@ import android.util.Log
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStream
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
 
 class DBHelper(private val context: Context, factory: SQLiteDatabase.CursorFactory?) :
     SQLiteOpenHelper(context, DB_NAME, factory, DB_VERSION) {
@@ -40,14 +48,15 @@ class DBHelper(private val context: Context, factory: SQLiteDatabase.CursorFacto
         val db = this.writableDatabase
         db.insert("USERS",null, values)
         db.close()
-        getUserData(username)
+        //sync(username)
+        parseXMLtoDB()
     }
     fun getUser(): Cursor? {
         val db = this.readableDatabase
         return db.rawQuery("SELECT * FROM USERS", null)
     }
 
-    fun getUserData(username: String){
+    fun sync(username: String){
         val queue = Volley.newRequestQueue(context)
         var url = "https://www.boardgamegeek.com/xmlapi2/collection?username=$username&stats=1"
         var responseString = ""
@@ -60,6 +69,36 @@ class DBHelper(private val context: Context, factory: SQLiteDatabase.CursorFacto
                 Log.e("MainActivity", "loadDogImage error: ${error.localizedMessage}")
             })
         queue.add(stringRequest)
+    }
+
+    fun parseXMLtoDB(){
+//        val inputStream: InputStream = context.assets.open("collection.xml")
+//        val size: Int = inputStream.available()
+//        val buffer = ByteArray(size)
+//        inputStream.read(buffer)
+//        var string = String(buffer)
+
+        val input: InputStream = context.assets.open("collection.xml")
+        val dbFactory: DocumentBuilderFactory = DocumentBuilderFactory.newInstance()
+        val dBuilder: DocumentBuilder = dbFactory.newDocumentBuilder()
+        //var xmlString = input.toString()
+        val doc: Document = dBuilder.parse(input)
+
+        val element: Element = doc.documentElement
+        element.normalize()
+
+        val nList: NodeList = doc.getElementsByTagName("item")
+
+        for (i in 0 until nList.length) {
+            val node: Node = nList.item(i)
+            if (node.nodeType == Node.ELEMENT_NODE) {
+                val element2 = node as Element
+                Log.d("TestValue", "value: " + element2.getElementsByTagName("name").item(0).textContent)
+//                tv1.setText(tv1.getText() + "\nName : " + getValue("name", element2) + "\n")
+//                tv1.setText(tv1.getText() + "Surname : " + getValue("surname", element2) + "\n")
+//                tv1.setText(tv1.getText() + "-----------------------")
+            }
+        }
     }
 
 }
