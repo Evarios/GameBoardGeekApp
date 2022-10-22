55d20388dec72e4f45be20cf69d84e935faf727d
Evarios <jan.brozdziak@gmail.com>
2022-10-22 21:43:40 +0200

    parsing xml to db

--- app/src/main/java/com/example/gameboardgeekapp/DBHelper.kt
+++ app/src/main/java/com/example/gameboardgeekapp/DBHelper.kt
@@ -28,7 +28,7 @@ class DBHelper(private val context: Context, factory: SQLiteDatabase.CursorFacto
         // along with their data types is given
         val queryCreateUsersTable = "CREATE TABLE USERS(ID INTEGER PRIMARY KEY, USERNAME TEXT, GAME_NUM INTEGER, EXPANSION_NUM INTEGER, LAST_SYNCED TEXT)"
         db.execSQL(queryCreateUsersTable)
-        val queryCreateGamesTable = "CREATE TABLE GAMES(ID INTEGER PRIMARY KEY, TITLE TEXT, RELEASE_YEAR INTEGER, RANKING INTEGER, IS_EXTENSION INTEGER, IMG_URL TEXT, LAST_SYNCED TEXT)"
+        val queryCreateGamesTable = "CREATE TABLE GAMES(ID INTEGER PRIMARY KEY, TITLE TEXT, RELEASE_YEAR INTEGER, BGG_ID INTEGER, RANKING INTEGER, IS_EXTENSION INTEGER, IMG_URL TEXT)"
         db.execSQL(queryCreateGamesTable)
     }
 
@@ -88,17 +88,47 @@ class DBHelper(private val context: Context, factory: SQLiteDatabase.CursorFacto
         element.normalize()
 
         val nList: NodeList = doc.getElementsByTagName("item")
-
+        var db = this.writableDatabase
+        db.execSQL("DELETE FROM GAMES")
         for (i in 0 until nList.length) {
             val node: Node = nList.item(i)
             if (node.nodeType == Node.ELEMENT_NODE) {
                 val element2 = node as Element
-                Log.d("TestValue", "value: " + element2.getElementsByTagName("name").item(0).textContent)
-//                tv1.setText(tv1.getText() + "\nName : " + getValue("name", element2) + "\n")
-//                tv1.setText(tv1.getText() + "Surname : " + getValue("surname", element2) + "\n")
-//                tv1.setText(tv1.getText() + "-----------------------")
+                val nameString = element2.getElementsByTagName("name").item(0).textContent
+                var yearPublishedString = ""
+                if(element2.getElementsByTagName("yearpublished").item(0) != null){
+                    yearPublishedString =
+                        element2.getElementsByTagName("yearpublished").item(0).textContent
+                } else
+                {
+
+                }
+                val imgUrlString = element2.getElementsByTagName("thumbnail").item(0).textContent
+                val bggIdString = element2.getAttribute("objectid")
+                val ratingItem = element2.getElementsByTagName("stats").item(0) as Element
+                val statsItem = ratingItem.getElementsByTagName("rating").item(0) as Element
+                val ranksItem = statsItem.getElementsByTagName("ranks").item(0) as Element
+                val rankItem = ranksItem.getElementsByTagName("rank").item(0) as Element
+                var rankString = rankItem.getAttribute("value")
+                if (rankString == "Not Ranked") {
+                    rankString = "0"
+                }
+                val values = ContentValues()
+                values.put("TITLE", nameString)
+                values.put("RELEASE_YEAR", if(yearPublishedString != "") yearPublishedString.toInt() else 0)
+                values.put("BGG_ID", bggIdString.toInt())
+                values.put("RANKING", rankString.toInt())
+                values.put("IMG_URL", imgUrlString)
+                db.insert("GAMES", null, values)
             }
         }
+        db.close()
+        db = this.readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM GAMES", null)
+        cursor.moveToFirst()
+        do{
+            Log.d("TestDB", cursor.getString(0) + " " + cursor.getString(1) + " " + cursor.getString(2))
+        } while (cursor.moveToNext())
     }
 
 }
